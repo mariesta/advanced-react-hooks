92fe855a03a10452ae0664429da8eec1f52bd1ed
Marie Starck <marie.starck@doctolib.com>
2022-10-19 10:55:14 +0200

    Epic React - Exercise 1

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -3,16 +3,23 @@
 
 import * as React from 'react'
 
-function Counter({initialCount = 0, step = 1}) {
-  // 🐨 replace React.useState with React.useReducer.
-  // 💰 React.useReducer(countReducer, initialCount)
-  const [count, setCount] = React.useState(initialCount)
+function countReducer(previousState, action) {
+  const {type, step} = action
+  switch (type) {
+    case 'INCREMENT':
+      return {...previousState, count: previousState.count + step}
+    default:
+      console.log(`${type} does not exist`);
+  }
+}
 
-  // 💰 you can write the countReducer function so you don't have to make any
-  // changes to the next two lines of code! Remember:
-  // The 1st argument is called "state" - the current value of count
-  // The 2nd argument is called "newState" - the value passed to setCount
-  const increment = () => setCount(count + step)
+function Counter({initialCount = 0, step = 1}) {
+  const [state, dispatch] = React.useReducer(countReducer, {
+    count: initialCount
+  })
+
+  const {count} = state
+  const increment = () => dispatch({type: 'INCREMENT', step})
   return <button onClick={increment}>{count}</button>
 }
 
